7585d8d90fb8d7d67345ed983ea22682486dd7a3
hayahussienn <hussienhaya99@gmail.com>
2025-02-18 11:13:03 +0200

    ..

--- src/test/java/com/example/catalog/HelloControllerTest.java
+++ src/test/java/com/example/catalog/HelloControllerTest.java
@@ -1,5 +1,8 @@
 package com.example.catalog;
 
+import io.qameta.allure.*;
+import jdk.jfr.Description;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,6 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static io.qameta.allure.SeverityLevel.CRITICAL;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -18,6 +22,13 @@ public class HelloControllerTest {
     private MockMvc mockMvc;
 
     @Test
+    @DisplayName("Test home endpoint")
+    @Description("Description............")
+    @Severity(CRITICAL)
+    @Owner("API Team")
+    @Link(name = "Website", url = "https://dev.example.com/")
+    @Issue("AUTH-123")
+    @TmsLink("TMS-456")
     public void testHome() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/"))
                 .andExpect(status().isOk())
